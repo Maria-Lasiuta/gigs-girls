2aa051e4b76e74467503b463e3d957c556dd3ab9
ChristinaDanylova <90180543+ChristinaDanylova@users.noreply.github.com>
2021-09-12 16:52:33 +0300

    Add files via upload

---
+++ ex.12.py
@@ -0,0 +1,3 @@
+kilo_grams = float (input("Enter the weight in kilograms: "))
+pounds = round(kilo_grams * 2.2046, 1)
+print(kilo_grams, 'Kilograms = ', pounds, 'Pounds')
---
+++ ex.2.py
@@ -0,0 +1,5 @@
+num = 3 and 5/8
+numerator = (3*8) + 5
+denominator = 8
+print("A floating point number = ", float(numerator/denominator))
+print("An integer number = ", int(numerator/denominator))
