7fa57fd5916a492dc2cd6f320071082fb727f33d
OlhaVeselovska <90180548+OlhaVeselovska@users.noreply.github.com>
2021-09-20 12:39:02 +0300

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,12 +1,10 @@
 '''Question 1
-Write a program that asks the user to enter a length in centimeters.
-If the user enters a negative length, the program should tell the user that
-the entry is invalid. Otherwise, the program should convert the length to
-inches and print out the result. There are 2.54 centimeters in an inch.'''
-length = float(input('Введіть довжину в см: '))
-if length < 0:
-    print('Запис невірний')
-else:
-    length = 2.54 * length
-    print(length)
+Write short scripts to print the results of the following expressions. In most places, changing integers to floating point produces a notably different result. For example (296167)2 and (296.0167.0)2. Use long as well as complex types to see the differences.
+355/113 * ( 1 - 0.0003/3522 )
+22/17 + 37/47 + 88/83
+(553/312)**2'''
+
+print(355/113 * ( 1 - 0.0003/3522 ))
+print(22/17 + 37/47 + 88/83)
+print((553/312)**2)
     
